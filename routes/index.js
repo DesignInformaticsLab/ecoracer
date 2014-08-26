9818e3a599ee608c6a5df94766f9b34332eacd22
yiren <renyi.tsinghua@gmail.com>
2014-08-26 14:00:58 -0400

    database updated

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ router.get('/db', function (req, res) {
       if (err)
        { console.error(err); res.send("Error " + err); }
       else
-       { res.render('db',{ data: result }); }
+       { res.render('db',{ data: JSON.stringify(result) }); }
     });
   });
 })
