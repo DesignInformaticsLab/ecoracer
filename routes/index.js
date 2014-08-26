bd5994cba7f2b5d5c2b4cafb51371a4417a3eeb3
yiren <renyi.tsinghua@gmail.com>
2014-08-26 11:12:16 -0400

    postgres added

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,20 @@ router.get('/ecoracer', function(req, res) {
     res.render('ecoracer', { title: 'Hello, World!' })
 });
 
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send(result.rows); }
+    });
+  });
+})
+
 ///* GET Userlist page. */
 //router.get('/userlist', function(req, res) {
 //    var db = req.db;
